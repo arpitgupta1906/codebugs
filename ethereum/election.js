047eb1bb06e0afca7038b5acdf9b2e86a9379b44
codelyf0505 <eat.sleep.code.repeat.0505@gmail.com>
2019-08-21 15:23:45 +0000

    projectuploades

--- ethereum/election.js
+++ ethereum/election.js
@@ -4,7 +4,7 @@ import Election from './build/Election.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(Election.interface),
-  '0x692a70d2e424a56d2c6c27aa97d1a86395877b3a'
+  '0x9e96074e565b087ec3c77968aae078788d988bf8'
 );
 
 export default instance;
